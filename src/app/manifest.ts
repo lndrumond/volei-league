602c6f40b01ebc54fa2c36fea849f58c31dcb51a
Leonardo <lndrumond@gmail.com>
2026-03-07 08:57:27 -0300

    Fix: Mudança icone do app e nome do app

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'AVL | Amigos do Vôlei League ',
-    short_name: 'Vôlei Areia',
+    short_name: 'Amigos do Vôlei',
     description: 'Gerenciador oficial das rodadas de vôlei do nosso grupo.',
     start_url: '/',
     display: 'standalone', // 🚨 ISSO AQUI TIRA A BARRA DO NAVEGADOR!
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#22c55e', // Verde principal pro topo do celular
     icons: [
       {
-        src: '/logo.png',
+        src: '/bola.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/logo.png',
+        src: '/bola.png',
         sizes: '512x512',
         type: 'image/png',
       },
